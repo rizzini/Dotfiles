2c4f53db0fa961f937cdf4eed245856ed78d4e2e
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-04-06 21:46:17 -0300

    Update macro_d3_wiz.sh

--- Documentos/scripts/macro_d3_wiz.sh
+++ Documentos/scripts/macro_d3_wiz.sh
@@ -3,13 +3,11 @@ set -x
 if [ $(pgrep macro_d3_wiz.sh | wc -l) -lt 3 ]; then
     while :;
     do
-        xdotool key 5;
-        
+        xdotool key 5;        
         sleep 0.4;
     done
 else
         killall -9 macro_d3_wiz.sh
-
 fi
 
 

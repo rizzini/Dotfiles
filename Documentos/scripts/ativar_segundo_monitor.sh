0238528a5a84f24847be8f7db2bffadacb9284fc
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-04-08 18:17:59 -0300

    ativa segundo monitor e altera o dispositivo de saído de som.

--- Documentos/scripts/ativar_segundo_monitor.sh
+++ Documentos/scripts/ativar_segundo_monitor.sh
@@ -8,9 +8,19 @@ fi
 
 if [ $(xrandr --listactivemonitors | head -1 | cut -c 11-11) -eq 1 ]; then
     xrandr --output HDMI-0 --auto --output HDMI-1-1 --same-as HDMI-0 --mode 1920x1080  
+    sleep 1
+    pacmd set-default-sink 1
+    sleep 1
+    pactl -- set-sink-volume 0 100%
+    sleep 1
+    pactl -- set-sink-volume 1 50%    
     notify-send "Segunda saída HDMI ativada"
 elif [ $(xrandr --listactivemonitors | head -1 | cut -c 11-11) -eq 2 ]; then
-    xrandr --output HDMI-0 --auto --output HDMI-1-1 --off  
+    pacmd set-default-sink 0
+    sleep 1
+    pactl -- set-sink-volume 0 50%
+    sleep 1
+    xrandr --output HDMI-0 --auto --output HDMI-1-1 --off      
     notify-send "Segunda saída HDMI desativada."
 fi
 

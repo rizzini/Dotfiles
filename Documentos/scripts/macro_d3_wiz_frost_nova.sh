21e28c7e04e60b9262462c32d5672e19dc7b1fc1
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-04-08 21:48:17 -0300

    novos macros

---
+++ Documentos/scripts/macro_d3_wiz_frost_nova.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+
+
+xdotool key 6;        
+sleep 0.1;
+xdotool key 6; 
+sleep 0.2;
+xdotool key 6; 
+sleep 0.3;
+xdotool key 6; 
---
+++ Documentos/scripts/macro_d3_wiz_mirror.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+
+
+xdotool key 7;        
+sleep 0.1;
+xdotool key 7; 
+sleep 0.2;
+xdotool key 7; 
+sleep 0.3;
+xdotool key 7; 

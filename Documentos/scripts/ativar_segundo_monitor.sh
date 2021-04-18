b41c17114c90b860bf8fb4c0cdc03d4296d43f03
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-04-18 04:20:43 -0300

    ..

--- Documentos/scripts/ativar_segundo_monitor.sh
+++ Documentos/scripts/ativar_segundo_monitor.sh
@@ -6,7 +6,7 @@ if [ ! $(xrandr | grep -w connected |  sed 's/ .*//' | wc -l) -eq 2 ]; then
     exit
 fi
 
-if [ $(xrandr --listactivemonitors | head -1 | cut -c 11-11) -eq 1 ]; then
+if [ -n "$(xrandr --listactivemonitors | grep 'HDMI-0')" ]; then
     xrandr --output HDMI-0 --off --output HDMI-1-1 --same-as HDMI-0 --mode 1920x1080  
 #     sleep 1
 #     pacmd set-default-sink 1
@@ -15,7 +15,7 @@ if [ $(xrandr --listactivemonitors | head -1 | cut -c 11-11) -eq 1 ]; then
 #     sleep 1
 #     pactl -- set-sink-volume 1 50%    
     notify-send "Segunda saída HDMI ativada"
-elif [ $(xrandr --listactivemonitors | head -1 | cut -c 11-11) -eq 2 ]; then
+elif [ -n "$(xrandr --listactivemonitors | grep 'HDMI-1-1')" ]; then
 #     pacmd set-default-sink 0
 #     sleep 1
 #     pactl -- set-sink-volume 0 50%

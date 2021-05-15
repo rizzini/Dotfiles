c9afce8465cd93c1832b56bdc88ffac1f511bc72
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-05-15 08:08:53 -0300

    corrigindo tradução com simbolo de porcentagem

--- Documentos/scripts/traduzir_clipboard.sh
+++ Documentos/scripts/traduzir_clipboard.sh
@@ -3,10 +3,14 @@ clipboard=$(xclip -o -sel clip)
 texto+=($clipboard)
 url_prefixo='https://translate.google.com/?sl=en&tl=pt&text='
 url_sufixo='&op=translate'
+re='^[0-9]+$'
 
 for i in "${texto[@]}"
-do
-   traduzir+="$i"'%20'
+do  
+    if [[ "$i" == *'%'* ]]; then
+        i=$(echo $i'25')
+    fi
+    traduzir+="$i"'%20'
 done
 
 google-chrome-stable --new-window $url_prefixo$traduzir$url_sufixo &

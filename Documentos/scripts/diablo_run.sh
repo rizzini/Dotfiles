418432fec92c768ee0f8682ce4b003f2d4e43a58
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-05-21 09:57:04 -0300

    update

--- Documentos/scripts/diablo_run.sh
+++ Documentos/scripts/diablo_run.sh
@@ -15,10 +15,10 @@ elif [ "$1" == 'US' ]; then
     WINEDEBUG=-all WINEFSYNC=0 WINEESYNC=0 WINEPREFIX=/home/lucas/.wine_d3/ gamemoderun /opt/wine_tkg/bin/wine /mnt/archlinux/Diablo\ III/Diablo\ III.exe -launch OnlineService.Matchmaking.ServerPool=Default &
 fi
 
-sleep 30 &&
+sleep 40 &&
 
 if [ -n "$(wmctrl -l | grep "Diablo III")" ]; then
-    killall plasmashell
+    killall -9 plasmashell
     /home/lucas/Documentos/scripts/c_diablo_xb.sh &
     xbindkeys &
 fi
@@ -27,12 +27,18 @@ while [ -n "$(wmctrl -l | grep "Diablo III")" ]; do
     sleep 1;
 done
 
+if [ ! "$(pgrep plasmashell)" ]; then
+    /usr/bin/plasmashell &> /dev/null & disown $! &
+fi
+
+sleep 1 &&
+
 if [[ $(dbus-send --session --dest=org.freedesktop.Notifications --print-reply /org/freedesktop/Notifications org.freedesktop.DBus.Properties.Get string:org.freedesktop.Notifications  string:Inhibited) == *"true"* ]]; then
     xdotool key XF86Calculator;
 fi
 
-/usr/bin/plasmashell &> /dev/null
-disown $!
+
+
 
 while [[ -n "$(pgrep c_diablo_xb.sh)" || -n "$(pgrep pqiv)" || -n "$(pgrep Diablo)" || -n "$(pgrep xdotool)" || -n "$(pgrep xbindkeys)" ]]; do
     killall -9 c_diablo_xb.sh pqiv xdotool xbindkeys;

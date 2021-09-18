2da8b86bea57b6f96e752c7205d5cfb72a066d69
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-09-18 16:20:47 -0300

    atualizando

--- Documentos/scripts/url_shortner.sh
+++ Documentos/scripts/url_shortner.sh
@@ -3,7 +3,11 @@ re='^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a
 url=$(xclip -o)
 if [[ $url =~ $re ]]; then 
     surl -c "$url" | xclip -selection clipboard
-    notify-send "Url encurtada com sucesso."
+    if [ $? -eq 0 ];then
+        notify-send "Url encurtada com sucesso."
+    else
+        notify-send "Link válido, mas houve algum problema ao encurtar o link"
+    fi
 else
     notify-send "Link inválido."
 fi

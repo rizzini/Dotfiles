b3bfa4ffc34999c2cf5908886c88099b0d8a873e
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-10-15 08:01:05 -0300

    atualizando

--- Documentos/scripts/aur_vote.sh
+++ Documentos/scripts/aur_vote.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 /usr/bin/gpg --decrypt-files  /home/lucas/Documentos/scripts/aur_vote.senha.gpg
 export AUR_AUTO_VOTE_PASSWORD="$(cat /home/lucas/Documentos/scripts/aur_vote.senha)"
-aur-auto-vote lucasrizzini &&
 rm /home/lucas/Documentos/scripts/aur_vote.senha
+aur-auto-vote lucasrizzini &&
+
 
 
 

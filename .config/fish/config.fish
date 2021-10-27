85d763970f5811543a959de9b0018946e3a3b89b
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-10-27 07:33:13 -0300

    atualizando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,6 +21,7 @@ if ! set -q (lspci -v | grep 'Kernel driver in use: nvidia') ;
     set -x __GL_MaxFramesAllowed 1
     set -x KWIN_COMPOSE O2ES
     set -x __GL_THREADED_OPTIMIZATION 1
+    set -x KWIN_OPENGL_INTERFACE egl
     set -x __GL_SHADER_DISK_CACHE 1
     set -x __GL_SHADER_DISK_CACHE_PATH '/home/lucas/.cache/'
     set -x KWIN_TRIPLE_BUFFER 1

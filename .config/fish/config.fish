bf1e7e861a9b643c7ba00a9cfc0d6a753c05ec23
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-10-28 20:11:50 -0300

    atualizando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -38,35 +38,6 @@ end
 ## Starship prompt
 source ("/usr/bin/starship" init fish --print-full-init | psub);
 
-
-## Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
-function __history_previous_command
-  switch (commandline -t)
-  case "!"
-    commandline -t $history[1]; commandline -f repaint
-  case "*"
-    commandline -i !
-  end
-end
-
-function __history_previous_command_arguments
-  switch (commandline -t)
-  case "!"
-    commandline -t ""
-    commandline -f history-token-search-backward
-  case "*"
-    commandline -i '$'
-  end
-end
-
-if [ "$fish_key_bindings" = fish_vi_key_bindings ];
-  bind -Minsert ! __history_previous_command
-  bind -Minsert '$' __history_previous_command_arguments
-else
-  bind ! __history_previous_command
-  bind '$' __history_previous_command_arguments
-end
-
 alias k=kate;
 alias plasmashell='killall plasmashell & sleep 1 && /usr/bin/plasmashell &> /dev/null & disown $last_pid';
 alias ls='exa -al --color=always --group-directories-first --icons';

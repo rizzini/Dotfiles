1407f1f5f667f92cf530c05d69677c8a20907e31
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-01 14:26:07 -0300

    atualizando

--- Documentos/scripts/youporn.sh
+++ Documentos/scripts/youporn.sh
@@ -2,25 +2,23 @@
 counter=0
 mkdir -p /home/lucas/Downloads/xxx/
 while :;do
-    counter=$(($counter + 1))
-    test -f /tmp/antigo && touch /tmp/antigo
+historico=''
+    counter=$(($counter + 1)) 
     atual=$(xclip -o)
-    antigo=$(cat /tmp/antigo)
     regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
     if [[ $atual =~ $regex ]];    then 
-        echo "$antigo" &> /tmp/antigo
-        if [[ ! "$antigo" == *"$atual"*  ]];then
-            cat $atual &> /tmp/antigo
+        grep -q "$(echo "$atual" | cut -d "=" -f2 )" /tmp/historico
+        if [ "$?" == "1" ];then
+            echo "$atual" | cut -d "=" -f2  >> /tmp/historico
             cd /home/lucas/Downloads/xxx/
             youtube-dl  "$atual" &> /dev/null &
             disown $!
             timeout 1 cvlc /home/lucas/Documentos/scripts/youporn.mp3 &> /dev/null &
         fi
-    
+
     fi    
 if [ $((counter%2)) -eq 0 ];
-then
-    
+then    
     echo -ne "Downloads em andamento: ""$(($(ps aux | grep yout | grep -v color | wc -l) - 1))\033[0K\r"
 fi
 sleep 1

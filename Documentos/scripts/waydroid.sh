d40d28a3dfca34e37473fd774606404ed5d7d938
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-01 08:22:18 -0300

    atualizando

--- Documentos/scripts/waydroid.sh
+++ Documentos/scripts/waydroid.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ "$(systemctl is-active waydroid-container.service)" == 'active' ];then
+    killall -9 weston
     sudo systemctl stop waydroid-container.service
     exit 0
 fi
+killall -9 weston
 sudo systemctl restart waydroid-container.service
 if [ -z "$(pgrep weston)" ]; then
     weston --xwayland &> /dev/null &
@@ -10,7 +12,7 @@ fi
 sleep 2 &&
 export XDG_SESSION_TYPE='wayland'
 export DISPLAY=':1'
-konsole --new-tab --hide-tabbar --hide-menubar -e '/usr/bin/waydroid show-full-ui'
+konsole --new-tab --hide-tabbar --hide-menubar -e '/usr/bin/waydroid show-full-ui' &
 while [ -n "$(pgrep weston)" ];do
     sleep 1
 done

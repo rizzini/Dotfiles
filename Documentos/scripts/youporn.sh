e1f74f94d70de54eed1caa3c87d908d781b9f9d9
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-02 11:29:57 -0300

    atualizando

--- Documentos/scripts/youporn.sh
+++ Documentos/scripts/youporn.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 counter=0
-mkdir -p /home/lucas/Downloads/xxx/xvideos
-mkdir -p /home/lucas/Downloads/xxx/xnxx
-mkdir -p /home/lucas/Downloads/xxx/pornhub
 while :;do
 historico=''
     counter=$(($counter + 1)) 
@@ -10,37 +7,41 @@ historico=''
     regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
     if [[ $atual =~ $regex ]] && [[ $atual == *"pornhub"* || $atual == *"xvideos"* || $atual == *"xnxx"* || $atual == *"xhamster"* ]];    then 
         if [[ $atual == *"pornhub"* ]];then
-            grep -q "$(echo "$atual" | cut -d "=" -f2 )" /home/lucas/Documentos/scripts/youporn_historico
+            grep -q "$(echo "$atual" | cut -d "=" -f2 )" /home/lucas/Documentos/scripts/pornhub_historico
             if [ "$?" == "1" ];then
-                echo "$atual" | cut -d "=" -f2  >> /home/lucas/Documentos/scripts/youporn_historico
+                mkdir -p /home/lucas/Downloads/xxx/pornhub
+                echo "$atual" | cut -d "=" -f2  >> /home/lucas/Documentos/scripts/pornhub_historico
                 cd /home/lucas/Downloads/xxx/pornhub/
                 youtube-dl  "$atual" &> /dev/null &
                 disown $!
-                timeout 1 mpg123 /home/lucas/Documentos/scripts/youporn.mp3 &> /dev/null &
+                timeout 1 mpg123 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null &
             fi 
         elif [[ $atual == *"xvideos"* ]];then
             grep -q "$(echo "$atual" | cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xvideos_historico
             if [ "$?" == "1" ];then
+                mkdir -p /home/lucas/Downloads/xxx/xvideos
                 echo "$atual" | cut -d "/" -f5  >> /home/lucas/Documentos/scripts/xvideos_historico
                 cd /home/lucas/Downloads/xxx/xvideos/
                 youtube-dl  "$atual" &> /dev/null &
                 disown $!
-                timeout 1 mpg123 /home/lucas/Documentos/scripts/youporn.mp3 &> /dev/null &
+                timeout 1 mpg123 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null &
             fi
         elif [[ $atual == *"xnxx"* ]];then
             grep -q "$(echo "$atual" | cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xnxx_historico
             if [ "$?" == "1" ];then
+                mkdir -p /home/lucas/Downloads/xxx/xnxx
                 echo "$atual" | cut -d "/" -f5  >> /home/lucas/Documentos/scripts/xnxx_historico
                 cd /home/lucas/Downloads/xxx/xnxx/
                 youtube-dl  "$atual" &> /dev/null &
                 disown $!
-                timeout 1 mpg123 /home/lucas/Documentos/scripts/youporn.mp3 &> /dev/null &
+                timeout 1 mpg123 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null &
             fi
         elif [[ $atual == *"xhamster"* ]];then
             grep -q "$(echo "$atual" | cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xhamster_historico
             if [ "$?" == "1" ];then
+                mkdir -p /home/lucas/Downloads/xxx/xhamster
                 echo "$atual" | cut -d "/" -f5  >> /home/lucas/Documentos/scripts/xhamster_historico
-                cd /home/lucas/Downloads/xxx/xnxx/
+                cd /home/lucas/Downloads/xxx/xhamster/
                 youtube-dl  "$atual" &> /dev/null &
                 disown $!
                 timeout 1 mpg123 /home/lucas/Documentos/scripts/youporn.mp3 &> /dev/null &

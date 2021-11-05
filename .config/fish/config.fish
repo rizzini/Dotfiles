583ac2aea65b31a295e1549c31eaec7527966887
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-04 23:21:21 -0300

    atualizando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,28 +13,12 @@ end
 
 export PATH="/opt/VirtualGL/bin/:$PATH";
 
-# set -x WINEFSYNC 1;
-# set -x WINEESYNC 1;
-
-# if ! set -q (lspci -v | grep 'Kernel driver in use: nvidia') ;
-#     set -x __GL_YIELD USLEEP
-#     set -x __GL_MaxFramesAllowed 1
-#     set -x KWIN_COMPOSE O2ES
-#     set -x __GL_THREADED_OPTIMIZATION 1
-#     set -x KWIN_OPENGL_INTERFACE egl
-#     set -x __GL_SHADER_DISK_CACHE 1
-#     set -x __GL_SHADER_DISK_CACHE_PATH '/home/lucas/.cache/'
-#     set -x KWIN_TRIPLE_BUFFER 1
-# end
-
-## Add ~/.local/bin to PATH
 if test -d ~/.local/bin
     if not contains -- ~/.local/bin $PATH
         set -p PATH ~/.local/bin
     end
 end
 
-## Starship prompt
 source ("/usr/bin/starship" init fish --print-full-init | psub);
 
 alias k=kate;
@@ -48,7 +32,6 @@ alias jctl="journalctl -p 3 -xb";
 alias syadm="sudo yadm -Y /etc/yadm";
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl";
 alias disown='disown $last_pid';
-alias transmission-cli='transmission-cli --download-dir=/home/lucas/Downloads/';
 alias cat='bat -p ';
 alias torrent='transmission-cli --download-dir=/home/lucas/Downloads/ '
 ## Import colorscheme from 'wal' asynchronously

afbeadc3265a6f3dd3ed7e8623fd4fbc7e612455
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-04 23:44:13 -0300

    atualizando

--- .config/fish/functions/findall.fish
+++ .config/fish/functions/findall.fish
@@ -1,3 +1,3 @@
 function findall
-sudo find /mnt/archlinux/ROOT/* -iname "*$argv[1]*" -path /refind_btrfs_rw_snapshots -prune
+    sudo find /mnt/archlinux/ROOT/* -iname "*$argv[1]*" -not -path "*refind_btrfs_rw_snapshots*" | less
 end

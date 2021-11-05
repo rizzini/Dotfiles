084314b9f3692616d66d2dc408b4ac41efe2afbd
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-05 12:56:10 -0300

    atualizando

--- Documentos/scripts/photoshop.sh
+++ Documentos/scripts/photoshop.sh
@@ -3,5 +3,5 @@ if [ -n "$1" ];then
     path=$(echo "Z:$1" | sed '\,/$, !s,$,/,;\,^/, s,/,:/,2;s,^/,,;s,/,\\,g' | rev | cut -c2- | rev)
     WINEPREFIX=/home/lucas/.wine_photoshop/ wine "c:\Program Files\Adobe\Adobe Photoshop CS6\Photoshop.exe" "$path" &> /dev/null 
 else
-    WINEPREFIX=/home/lucas/.wine_photoshop/ wine /home/lucas/.wine_photoshop/drive_c/Program\ Files/Adobe/Adobe\ Photoshop\ CS6/Photoshop.exe &> /dev/null 
+    WINEPREFIX=/home/lucas/.wine_photoshop/ wine "c:\Program Files\Adobe\Adobe Photoshop CS6\Photoshop.exe" &> /dev/null 
 fi

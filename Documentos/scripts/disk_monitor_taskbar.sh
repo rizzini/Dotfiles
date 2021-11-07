45288d1f0d6a9a285cd1b607469640d918544f54
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-07 08:16:07 -0300

    atualizando

--- Documentos/scripts/disk_monitor_taskbar.sh
+++ Documentos/scripts/disk_monitor_taskbar.sh
@@ -2,11 +2,9 @@
 if [ -b "/dev/sdb" ]; then
     sdb="1"
 fi
-
 if [ -b "/dev/sdc" ]; then
     sdc="1"
-fi
-    
+fi    
 data1_read_sda2=$(cat /proc/diskstats | grep -w sda | awk '{print $6}');
 data1_write_sda2=$(cat /proc/diskstats | grep -w sda | awk '{print $10}');
 if [ -n "$sdb" ]; then
@@ -17,9 +15,7 @@ if [ -n "$sdc" ]; then
     data1_read_sdc1=$(cat /proc/diskstats | grep -w sdc | awk '{print $6}');
     data1_write_sdc1=$(cat /proc/diskstats | grep -w sdc | awk '{print $10}');
 fi
-
 sleep 1 &&
-
 data2_read_sda2=$(cat /proc/diskstats | grep -w sda | awk '{print $6}');
 data2_write_sda2=$(cat /proc/diskstats | grep -w sda | awk '{print $10}');
 read_sda2=$((($data2_read_sda2 - $data1_read_sda2) / 2));
@@ -36,8 +32,6 @@ if [ -n "$sdc" ]; then
     read_sdc1=$((($data2_read_sdc1 - $data1_read_sdc1) / 2));
     write_sdc1=$((($data2_write_sdc1 - $data1_write_sdc1) / 2));
 fi
-
-
 printf " SSD|""R: "$(echo $(( ${read_sda2%% *} / 1024))"MB/s")" W: "$(echo "$(( ${write_sda2%% *} / 1024))MB/s")"\n" ;
 if [ -n "$sdb" ]; then
     printf "sdb|""R: "$(echo "$(( ${read_sdb%% *} / 1024))MB/s")" W: "$(echo "$(( ${write_sdb%% *} / 1024))MB/s");

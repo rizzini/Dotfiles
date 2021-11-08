ac719f8b2f6121343d9cb66ca0a36af186d74229
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-07 21:01:08 -0300

    atualizando

--- Documentos/scripts/perfil_fone_de_ouvido_bluetooth.sh
+++ Documentos/scripts/perfil_fone_de_ouvido_bluetooth.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 while :; do
-    while [ $(ls /sys/class/bluetooth | wc -l) -eq 3 ];do
+    while [ $(ls /sys/class/bluetooth | wc -l) -ge 2 ];do
         if [ -n "$(pactl list | grep "Perfil ativo: headset_head_unit")" ];then
-            sleep 1
+            sleep 5
             pacmd set-card-profile bluez_card.FC_58_FA_93_42_42 a2dp_sink_sbc
+        fi
+        sleep 2
+        if [ -n "$(pactl list | grep "Perfil ativo: a2dp_sink_sbc")" ];then
+            break
         fi
     done
     sleep 1

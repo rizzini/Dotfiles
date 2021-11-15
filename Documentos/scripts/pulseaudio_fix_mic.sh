dc404173bb308d5af9c0a2f418ce40cc6f4edb79
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-14 22:21:53 -0300

    atualizando

--- Documentos/scripts/pulseaudio_fix_mic.sh
+++ Documentos/scripts/pulseaudio_fix_mic.sh
@@ -3,12 +3,12 @@
 set -x
 contador=0
 if [ -z "$(/usr/bin/ls /dev/dri/ | /usr/bin/grep card1)" ];then
-    if [ -z "$(pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];then
+    if [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 0;
         /usr/bin/systemctl --user restart pulseaudio.service &
         /usr/bin/sleep 2
     fi
-    if [ -z "$(pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];then
+    if [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 1;
         /usr/bin/systemctl --user restart pulseaudio.service &
     fi

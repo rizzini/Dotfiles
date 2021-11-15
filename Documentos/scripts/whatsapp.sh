32d395504e7aba14eacafc6fa93ad766511ba5d4
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-15 06:24:35 -0300

    atualizando

--- Documentos/scripts/whatsapp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ -z "$(pgrep WhatsApp)" ]; then
-    rm -rf /home/lucas/.config/whatsapp-nativefier-d40211
-    cp -r /home/lucas/Documentos/scripts/whatsapp-nativefier-d40211 /home/lucas/.config/whatsapp-nativefier-d40211;
-fi
-/usr/bin/whatsapp-nativefier &      

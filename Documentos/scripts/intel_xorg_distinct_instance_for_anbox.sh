5c6ffcf5256f18b8768513195e877ee6bdbde26a
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-15 06:44:55 -0300

    atualizando

--- Documentos/scripts/intel_xorg_distinct_instance_for_anbox.sh
+++ Documentos/scripts/intel_xorg_distinct_instance_for_anbox.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-if [ -z "$(lspci | grep '00:02.0 Display controller')" ];then
-    sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send  'iGPU desabilitada..'
+if [ -z "$(/usr/bin/lspci | /usr/bin/grep '00:02.0 Display controller')" ];then
+    /usr/bin/sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send  'iGPU desabilitada..'
     exit 1
 fi
-if [ "$(systemctl is-active intel_display.service)" == "active" ];then
-    sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send  'Anbox encerrado..'
-    systemctl stop intel_display.service 
-    systemctl stop anbox-container-manager.service
-elif [[ "$(systemctl is-active intel_display.service)" == "inactive" || "$(systemctl is-active intel_display.service)" == "failed" ]];then
-    sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send  'Anbox iniciado..'
-    systemctl restart anbox-container-manager.service
-    systemctl restart intel_display.service 
-    while [ -z "$(adb shell ps | grep whatsapp)" ];do
-        sleep 1
+if [ "$(/usr/bin/systemctl is-active intel_display.service)" == "active" ];then
+    /usr/bin/sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send  'Anbox encerrado..'
+    /usr/bin/systemctl stop intel_display.service 
+    /usr/bin/systemctl stop anbox-container-manager.service
+elif [[ "$(/usr/bin/systemctl is-active intel_display.service)" == "inactive" || "$(/usr/bin/systemctl is-active intel_display.service)" == "failed" ]];then
+    /usr/bin/sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send  'Anbox iniciado..'
+    /usr/bin/systemctl restart anbox-container-manager.service
+    /usr/bin/systemctl restart intel_display.service 
+    while [ -z "$(/usr/bin/adb shell ps | /usr/bin/grep whatsapp)" ];do
+        /usr/bin/sleep 1
     done
-    sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send  'WhatsApp inciado..'
+    /usr/bin/sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus /usr/bin/notify-send  'WhatsApp inciado..'
 fi

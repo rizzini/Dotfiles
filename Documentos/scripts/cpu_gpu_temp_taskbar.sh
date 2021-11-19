8fb004984f2f4a8eccd01b5c888e967230f3c079
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-19 12:40:52 -0300

    atualizando

--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/printf "CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc"
+/usr/bin/printf "CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc | Free space: $(df -h | grep sda2 | head -1 | awk '{print $5}')%"
 #" | " 
 
 

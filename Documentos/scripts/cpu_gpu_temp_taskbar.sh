5902dd32e478fd2cb97857befb8678946ba2ac41
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-19 12:41:42 -0300

    atualizando

--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-/usr/bin/printf "CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc | Free space: $(df -h | grep sda2 | head -1 | awk '{print $5}')%"
-#" | " 
-
-
-# if [ -z "$(/usr/bin/lsmod | /usr/bin/grep nouveau)" ]; then
-#     /usr/bin/printf "GPU: ""$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-17)""ºc"""
-# else
-#     temp_gpu=$(/usr/bin/nvidia-smi --query-gpu=temperature.gpu,utilization.gpu --format=csv,nounits,noheader | /usr/bin/cut -c1-2)
-#     if [ $temp_gpu -ge 70 ];then
-#         /usr/bin/printf "GPU: ""$(/usr/bin/echo 'ALERTA!')""\n"
-#     else
-#         /usr/bin/printf "GPU: ""$(/usr/bin/echo $temp_gpu)""ºc""\n"
-#     fi
-# fi
+/usr/bin/printf "Free space: $(df -h | grep sda2 | head -1 | awk '{print $5}')% | CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc"

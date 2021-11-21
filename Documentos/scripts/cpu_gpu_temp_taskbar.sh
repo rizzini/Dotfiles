e2071e04f87c4b78e411e8f547db25abbb85da42
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-21 16:46:22 -0300

    atualizando

--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -4,7 +4,12 @@ if [ -n "$(pgrep amule)" ];then
 else
     /usr/bin/printf "CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc | "
     if [ -n "$(/usr/bin/lsmod | /usr/bin/grep nouveau)" ]; then
-        /usr/bin/printf "GPU: ""$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-17)""ºc"""
+        temp_gpu=$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/head -1 | /usr/bin/cut -c 16-17)
+        if [ $temp_gpu -ge 70 ];then
+            /usr/bin/printf "GPU: ""$(/usr/bin/echo 'ALERTA!')""\n"
+        else
+            /usr/bin/printf "GPU: ""$(echo $temp_gpu)""ºc"""
+        fi
     else
         temp_gpu=$(/usr/bin/nvidia-smi --query-gpu=temperature.gpu,utilization.gpu --format=csv,nounits,noheader | /usr/bin/cut -c1-2)
     if [ $temp_gpu -ge 70 ];then

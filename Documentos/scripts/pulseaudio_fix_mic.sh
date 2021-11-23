9efda30e97ba287df46df78c6b7bb911b6454bb0
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-23 09:03:59 -0300

    atualizando

--- Documentos/scripts/pulseaudio_fix_mic.sh
+++ Documentos/scripts/pulseaudio_fix_mic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 contador=0
-while [ -n "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];do
+while [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];do
     if [ -z "$(/usr/bin/ls /dev/dri/ | /usr/bin/grep card1)" ];then
         if [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];then
             /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 0;
@@ -16,23 +16,4 @@ while [ -n "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = 
     fi  
     sleep 1
 done
-if [[ -n "$( /usr/bin/pactl list sources | /usr/bin/grep 'hw:2')" || -n "$( /usr/bin/pactl list sources | /usr/bin/grep 'hw:1')" ]];then
-    if [ -z "$( /usr/bin/pactl list sources | /usr/bin/grep -E 'USB|CAMERA')" ];then
-        exit;
-    fi
-fi
-while :;do
-    contador=$(($contador + 1))
-    if [ `/usr/bin/expr $contador % 2` == 0 ];then
-        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 2;
-    else
-        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 1;
-    fi
-    /usr/bin/pulseaudio -k;
-    /usr/bin/sleep 1
-    if [[ -n "$( /usr/bin/pactl list sources | /usr/bin/grep 'hw:2')" || -n "$( /usr/bin/pactl list sources | /usr/bin/grep 'hw:1')" ]];then
-        if [ -z "$( /usr/bin/pactl list sources | /usr/bin/grep -E 'USB|CAMERA')" ];then
-            break;
-        fi
-    fi
-done
+

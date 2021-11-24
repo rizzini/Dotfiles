48f50dc418b7e0d557f2ef672890c1466cc112bf
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-24 11:17:42 -0300

    atualizando

--- Documentos/scripts/pulseaudio_fix_mic.sh
+++ Documentos/scripts/pulseaudio_fix_mic.sh
@@ -2,18 +2,17 @@
 set -x
 contador=0
 while [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];do
-    if [ -z "$(/usr/bin/ls /dev/dri/ | /usr/bin/grep card1)" ];then
-        if [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];then
-            /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 0;
-            /usr/bin/systemctl --user restart pulseaudio.service &
-            /usr/bin/sleep 2
-        fi
-        if [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = "Áudio interno"')" ];then
-            /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 1;
-            /usr/bin/systemctl --user restart pulseaudio.service &
-        fi    
-        exit;
-    fi  
-    sleep 1
+    contador=$(($contador + 1))
+    if [ $contador -eq 2 ];then
+        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 0;
+        /usr/bin/systemctl --user restart pulseaudio.service
+    elif [ $contador -eq 3 ];then
+        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 1;  
+        /usr/bin/systemctl --user restart pulseaudio.service
+    elif [ $contador -eq 4 ];then
+        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 2;  
+        /usr/bin/systemctl --user restart pulseaudio.service
+    fi    
+    /usr/bin/sleep 1
 done
 

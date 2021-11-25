4af5ddb9bc37775ea6adb5173cbecc9f73621968
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-24 23:11:43 -0300

    atualizando

---
+++ Documentos/scripts/switch_xorg_nvidia_and_intel_card.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ "$1" == 'nvidia' ];then 
+    if [ -z "$(grep nvidia /etc/X11/xorg.conf)" ];then
+        cp /etc/X11/nvidia/xorg.conf /etc/X11/xorg.conf;
+        systemctl restart sddm
+    else
+        /usr/bin/sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send 'Você já usando NVIDIA.'
+    fi
+elif [ "$1" == 'intel' ];then 
+    if [ -n "$(grep nvidia /etc/X11/xorg.conf)" ];then
+        cp /etc/X11/intel/xorg.conf /etc/X11/xorg.conf;
+        systemctl restart sddm
+    else
+        /usr/bin/sudo -u lucas XDG_RUNTIME_DIR=/run/user/1000 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send 'Você já usando Intel.'
+    fi
+fi

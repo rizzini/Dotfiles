4db6f6ad06e7a6bd095c281b48ba97175517bfa0
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-25 11:27:15 -0300

    atualizando

--- Documentos/scripts/identifica_texto_copia_para_area_de_transferencia.sh
+++
@@ -1,12 +0,0 @@
-#!/bin/bash
-# Dependencies: scrot tesseract-ocr imagemagick
-language=${1:-eng}
-# set -e
-image_file=/tmp/clipboard.png
-output_file=/tmp/clipboard
-trap 'rm $image_file*' EXIT
-/usr/bin/scrot -a $(/usr/bin/slop -f '%x,%y,%w,%h') "$image_file"
-/usr/bin/mogrify -modulate 100,0 -resize 400% "$image_file" # should increase detection rate
-/usr/bin/tesseract -l $language "$image_file" "$output_file" &> /dev/null
-/usr/bin/cat $output_file.txt | /usr/bin/xclip -se c
-

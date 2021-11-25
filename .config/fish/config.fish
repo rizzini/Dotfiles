0c24468db7917f8dc30578623b439c237cdfc0ab
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-11-25 06:46:26 -0300

    atualizando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,23 @@ set VIRTUAL_ENV_DISABLE_PROMPT "1";
 source ~/.profile;
 
 set XORG (grep nvidia /etc/X11/xorg.conf)
-if test -n "$DISPLAY"
-    if test "$DISPLAY" = ":0"; and ! set -q $XORG;
+if test -n "$DISPLAY";
+    if ! set -q $XORG;
         if ! string match -q "*opengl_libs*" $LD_LIBRARY_PATH
             set -x LD_LIBRARY_PATH "$LD_LIBRARY_PATH:/mnt/archlinux/opengl_libs/nvidia/";
         end
+        if test -d "$HOME/.cache/";
+            export __GL_SHADER_DISK_CACHE_PATH="$HOME/.cache/";
+        else
+            mkdir -p $HOME/.cache/
+            export __GL_SHADER_DISK_CACHE_PATH="$HOME/.cache/";
+        end
+        export KWIN_TRIPLE_BUFFER=1;
+        export __GL_YIELD=USLEEP;
+        export __GL_MaxFramesAllowed=1;
+        export KWIN_COMPOSE=O2ES;
+        export __GL_THREADED_OPTIMIZATION=1;
+        export __GL_SHADER_DISK_CACHE=1;
     end
 end
 

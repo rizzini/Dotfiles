6f20bc9c118312edc07234963a768c50d4cbbb2e
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-12-01 18:02:27 -0300

    atualizando

--- Documentos/scripts/traduzir_clipboard.sh
+++ Documentos/scripts/traduzir_clipboard.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 clipboard=$(/usr/bin/xclip -o -sel clip)
-texto+=($clipboard)
-url_prefixo='https://translate.google.com/?sl=en&tl=pt&text='
-url_sufixo='&op=translate'
-#corrigir tradução quando após % vier um ponto. Ex.: it wasn't 100%.
-
-for i in "${texto[@]}"
-do  
-    if [[ "$i" == *'%'* ]]; then
-        i=$(/usr/bin/echo $i'25')
-    fi
-    traduzir+="$i"'%20'
-done
-
-/usr/bin/xdg-open $url_prefixo$traduzir$url_sufixo &
+if [ -z "$clipboard" ];then
+    exit
+fi
+kdialog --title 'Google Translator' --textinputbox 'Google Translator' "$(/usr/bin/crow -b $clipboard)"

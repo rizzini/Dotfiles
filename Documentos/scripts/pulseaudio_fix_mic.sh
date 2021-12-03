ef247c5e2c4cc86cc0566a7b5083aea6cbcbbb00
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-12-03 09:29:46 -0300

    atualizando

--- Documentos/scripts/pulseaudio_fix_mic.sh
+++ Documentos/scripts/pulseaudio_fix_mic.sh
@@ -5,20 +5,11 @@ while [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = 
     if [[ $contador -eq 1 && -z "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 0_intel;
         /usr/bin/systemctl --user restart pulseaudio.service
-    elif [[ $contador -eq 1 && -n "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
-        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 0_nvidia;
-        /usr/bin/systemctl --user restart pulseaudio.service    
     elif [[ $contador -eq 2 && -z "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 1_intel;  
         /usr/bin/systemctl --user restart pulseaudio.service
-    elif [[ $contador -eq 2 && -n "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then 
-        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 1_nvidia;  
-        /usr/bin/systemctl --user restart pulseaudio.service    
     elif [[ $contador -eq 3 && -z "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 2_intel;  
-        /usr/bin/systemctl --user restart pulseaudio.service
-    elif [[ $contador -eq 3 && -n "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
-        /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 2_nvidia;  
         /usr/bin/systemctl --user restart pulseaudio.service
     fi    
     /usr/bin/sleep 1
--- Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh
+++ Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 if [ "$1" == "0_intel" ]; then
     /usr/bin/cp /etc/pulse/default.pa_0_intel /etc/pulse/default.pa;
-elif [ "$1" == "0_nvidia" ]; then
-    /usr/bin/cp /etc/pulse/default.pa_0_nvidia /etc/pulse/default.pa;
 elif [ "$1" == "1_intel" ]; then
     /usr/bin/cp /etc/pulse/default.pa_1_intel /etc/pulse/default.pa;
-elif [ "$1" == "1_nvidia" ]; then
-    /usr/bin/cp /etc/pulse/default.pa_1_nvidia /etc/pulse/default.pa;
 elif [ "$1" == "2_intel" ]; then
     /usr/bin/cp /etc/pulse/default.pa_2_intel /etc/pulse/default.pa;
-elif [ "$1" == "2_nvidia" ]; then
-    /usr/bin/cp /etc/pulse/default.pa_2_nvidia /etc/pulse/default.pa;
 fi

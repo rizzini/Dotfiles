2e1b5cb6961d336c59dd2f394fd71d432f6a0290
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-12-09 16:24:31 -0300

    atualizando

--- Documentos/scripts/traduzir_clipboard.sh
+++ Documentos/scripts/traduzir_clipboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clipboard=$(/usr/bin/xclip -o -sel clip)
 if [ -n "$clipboard" ];then
-    kdialog --title 'Google Translator' --textinputbox 'Google Translator' "$(/usr/bin/crow -b $clipboard)";
+    kdialog --title 'Google Translator' --textinputbox 'Texto traduzido:' "$(/usr/bin/crow -b $clipboard)";
 fi
 

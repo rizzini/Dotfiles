ce84a040de4bcedbbc71e5f62672edda7ead6304
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-12-09 16:22:32 -0300

    atualizando

--- Documentos/scripts/pulseaudio_fix_mic.sh
+++ Documentos/scripts/pulseaudio_fix_mic.sh
@@ -4,13 +4,13 @@ while [ -z "$(/usr/bin/pactl list sources | /usr/bin/grep 'device.description = 
     contador=$(($contador + 1))
     if [[ $contador -eq 1 && -z "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 0_intel;
-        /usr/bin/systemctl --user restart pulseaudio.service
+        /usr/bin/systemctl --user restart pulseaudio.service;
     elif [[ $contador -eq 2 && -z "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 1_intel;  
-        /usr/bin/systemctl --user restart pulseaudio.service
+        /usr/bin/systemctl --user restart pulseaudio.service;
     elif [[ $contador -eq 3 && -z "$(/usr/bin/grep nvidia /etc/X11/xorg.conf)" ]];then
         /usr/bin/sudo /home/lucas/Documentos/scripts/pulseaudio_fix_mic_copy_files_sudo.sh 2_intel;  
-        /usr/bin/systemctl --user restart pulseaudio.service
+        /usr/bin/systemctl --user restart pulseaudio.service;
     fi    
-    /usr/bin/sleep 1
+    /usr/bin/sleep 1;
 done

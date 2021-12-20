bbbc04fc8599e1bed0339e6eb163e2a2496dd0e0
Lucas Rizzini <lucasrizzini@hotmail.com>
2021-12-20 12:05:50 -0300

    atualizando

--- Documentos/scripts/youporn.sh
+++ Documentos/scripts/youporn.sh
@@ -24,7 +24,7 @@ while :; do
                 cd /home/lucas/Downloads/xxx/pornhub/;
                 /usr/bin/yt-dlp "$clipboard" &> /dev/null &
                 disown $!;
-                /usr/bin/ffplay  -nodisp -autoexit -volume 50 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                /usr/bin/ffplay  -nodisp -autoexit -volume 20 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
                 clipboard=''
             fi 
         elif [[ $clipboard == *"xvideos"* ]]; then
@@ -35,7 +35,7 @@ while :; do
                 cd /home/lucas/Downloads/xxx/xvideos/;
                 /usr/bin/yt-dlp "$clipboard" &> /dev/null &
                 disown $!;
-                /usr/bin/ffplay  -nodisp -autoexit -volume 50 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                /usr/bin/ffplay  -nodisp -autoexit -volume 20 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
             fi
         elif [[ $clipboard == *"xnxx"* ]]; then
             /usr/bin/grep -q "$(/usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xnxx_historico;
@@ -45,7 +45,7 @@ while :; do
                 cd /home/lucas/Downloads/xxx/xnxx/;
                 /usr/bin/yt-dlp "$clipboard" &> /dev/null &
                 disown $!;
-                /usr/bin/ffplay  -nodisp -autoexit -volume 50 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                /usr/bin/ffplay  -nodisp -autoexit -volume 20 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
             fi
         elif [[ $clipboard == *"xhamster"* ]]; then
             /usr/bin/grep -q "$(/usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xhamster_historico;
@@ -55,7 +55,7 @@ while :; do
                 cd /home/lucas/Downloads/xxx/xhamster/;
                 /usr/bin/yt-dlp "$clipboard" &> /dev/null &
                 disown $!;
-                /usr/bin/ffplay  -nodisp -autoexit -volume 50 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                /usr/bin/ffplay  -nodisp -autoexit -volume 20 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
             fi
         fi
     fi    

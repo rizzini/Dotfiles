f01431af587024af33dcfb0f40d95686139e4ef4
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-01-07 10:39:07 -0300

    atualizando

--- Documentos/scripts/youporn.sh
+++ Documentos/scripts/youporn.sh
@@ -30,7 +30,7 @@ while :; do
                     fi
                 done
                 if [ $ja_baixado -eq 0 ];then
-                    /usr/bin/ffplay  -nodisp -autoexit -volume 80 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                    /usr/bin/ffplay  -nodisp -autoexit  -volume 40 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
                     /usr/bin/mkdir -p /home/lucas/Downloads/xxx/pornhub;
                     /usr/bin/echo "$clipboard" | /usr/bin/cut -d "=" -f2  >> /home/lucas/Documentos/scripts/pornhub_historico;
                     cd /home/lucas/Downloads/xxx/pornhub/;
@@ -42,7 +42,7 @@ while :; do
         elif [[ $clipboard == *"xvideos"* ]]; then
             /usr/bin/grep -q "$(/usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xvideos_historico;
             if [ "$?" == "1" ]; then
-                /usr/bin/ffplay  -nodisp -autoexit -volume 80 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                /usr/bin/ffplay  -nodisp -autoexit  -volume 40 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
                 /usr/bin/mkdir -p /home/lucas/Downloads/xxx/xvideos;
                 /usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5  >> /home/lucas/Documentos/scripts/xvideos_historico;
                 cd /home/lucas/Downloads/xxx/xvideos/;
@@ -52,7 +52,7 @@ while :; do
         elif [[ $clipboard == *"xnxx"* ]]; then
             /usr/bin/grep -q "$(/usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xnxx_historico;
             if [ "$?" == "1" ]; then
-                /usr/bin/ffplay  -nodisp -autoexit -volume 80 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                /usr/bin/ffplay  -nodisp -autoexit  -volume 40 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
                 /usr/bin/mkdir -p /home/lucas/Downloads/xxx/xnxx;
                 /usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5  >> /home/lucas/Documentos/scripts/xnxx_historico;
                 cd /home/lucas/Downloads/xxx/xnxx/;
@@ -62,7 +62,7 @@ while :; do
         elif [[ $clipboard == *"xhamster"* ]]; then
             /usr/bin/grep -q "$(/usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5 )" /home/lucas/Documentos/scripts/xhamster_historico;
             if [ "$?" == "1" ]; then
-                /usr/bin/ffplay  -nodisp -autoexit -volume 80 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
+                /usr/bin/ffplay  -nodisp -autoexit  -volume 40 /home/lucas/Documentos/scripts/notify.mp3 &> /dev/null & 
                 /usr/bin/mkdir -p /home/lucas/Downloads/xxx/xhamster;
                 /usr/bin/echo "$clipboard" | /usr/bin/cut -d "/" -f5  >> /home/lucas/Documentos/scripts/xhamster_historico;
                 cd /home/lucas/Downloads/xxx/xhamster/;
@@ -75,7 +75,7 @@ if [ $((counter%2)) -eq 0 ]; then
     /usr/bin/find /home/lucas/Downloads/xxx/ -iname "*.mp4" -exec mv {} /home/lucas/Downloads/megasync_temp/ \;
     /usr/bin/echo -ne "Espaço livre: $(/usr/bin/df -h | /usr/bin/grep sda2 | /usr/bin/head -1 | /usr/bin/awk '{print $4}') / Downloads em andamento: ""\033[1;35m$(($(/usr/bin/ps aux | /usr/bin/grep yt-dlp | /usr/bin/grep -v color | /usr/bin/wc -l) - 1))\r\033[0m";
 fi
-/usr/bin/sleep 1
+/usr/bin/sleep 0.5
 printf "$(ps auxm | grep 'yt-dlp' | awk '{print $13}')\n" &> /home/lucas/Downloads/xxx/links.txt
 killall -9 ffmpeg &> /dev/null &
 done

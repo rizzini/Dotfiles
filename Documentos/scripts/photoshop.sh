9e579d69b1abaf022deaf366c45c897157985baa
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-01-17 19:52:01 -0300

    atualizando

--- Documentos/scripts/photoshop.sh
+++ Documentos/scripts/photoshop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ -n "$1" ];then   
-    WINEPREFIX=/home/lucas/.wine_photoshop/ /opt/wine_tkg/bin/wine "c:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Photoshop.exe" "$(/usr/bin/echo "Z:$1" | /usr/bin/sed '\,/$, !s,$,/,;\,^/, s,/,:/,2;s,^/,,;s,/,\\,g' | /usr/bin/rev | /usr/bin/cut -c2- | /usr/bin/rev)" &> /dev/null 
+    WINEPREFIX=/home/lucas/.wine_photoshop/ wine_tkg "c:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Photoshop.exe" "$(/usr/bin/echo "Z:$1" | /usr/bin/sed '\,/$, !s,$,/,;\,^/, s,/,:/,2;s,^/,,;s,/,\\,g' | /usr/bin/rev | /usr/bin/cut -c2- | /usr/bin/rev)" &> /dev/null 
 else
-    WINEPREFIX=/home/lucas/.wine_photoshop/ /opt/wine_tkg/bin/wine "c:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Photoshop.exe" &> /dev/null 
+    WINEPREFIX=/home/lucas/.wine_photoshop/ wine_tkg "c:\Program Files\Adobe\Adobe Photoshop CS6 (64 Bit)\Photoshop.exe" &> /dev/null 
 fi
 
 

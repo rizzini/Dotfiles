df7c718f111ca38567623f24d35d690c695965e7
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-01-18 10:43:24 -0300

    atualizando

--- Documentos/scripts/traduzir_clipboard.sh
+++ Documentos/scripts/traduzir_clipboard.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 clipboard=$(/usr/bin/xclip -o -sel clip)
+pointer_x=$(xdotool getmouselocation | cut -c3-6 | tr -d 'y:' | tr -d ' ')
+pointer_y=$(xdotool getmouselocation | grep -Po 'y:\K[^"][^"][^"][^"]' | tr -d 'sc' | tr -d ' ')
 if [ -n "$clipboard" ];then
-    kdialog --title 'Google Translator' --textinputbox 'Texto traduzido:' "$(/usr/bin/crow -b $clipboard)";
+    kdialog --title 'Google Translator' --geometry=400x200+$pointer_x+$pointer_y --textinputbox 'Texto traduzido:' "$(/usr/bin/crow -b $clipboard)";
 fi
 

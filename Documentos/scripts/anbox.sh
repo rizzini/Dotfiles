75e51551b226a70b6ff55d2bb42aa97b88345b59
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-01-19 15:42:43 -0300

    atualizando

--- Documentos/scripts/anbox.sh
+++ Documentos/scripts/anbox.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-if [ "$(/usr/bin/systemctl is-active anbox-container-manager.service)" == 'active' ];then
+if [ "$(/usr/bin/systemctl is-active anbox.service)" == 'active' ];then
     /usr/bin/sudo /usr/bin/systemctl stop anbox.service;
-    /usr/bin/sudo /usr/bin/systemctl stop anbox-container-manager.service;
 else
     /usr/bin/sudo /usr/bin/systemctl restart anbox.service;
 fi

3d2192fd0b58a9e5b97c7ce17cd8900873e9cf22
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-01-22 18:19:10 -0300

    atualizando

--- Documentos/scripts/anbox.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$(/usr/bin/systemctl is-active anbox.service)" == 'active' ];then
-    /usr/bin/sudo /usr/bin/systemctl stop anbox.service;
-else
-    /usr/bin/sudo /usr/bin/systemctl restart anbox.service;
-fi

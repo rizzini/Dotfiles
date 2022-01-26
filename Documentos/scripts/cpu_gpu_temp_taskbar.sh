c95d53e08896c8a0bb517f78d419bc22456a2982
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-01-26 20:35:19 -0300

    atualizando

--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-if [ -n "$(/usr/bin/pgrep yt-dlp)" ];then
-    /home/lucas/Documentos/scripts/youporn.sh taskbar
-else
-    if [ -n "$(/usr/bin/pgrep amule)" ];then
-        /usr/bin/printf "Free space: $(/usr/bin/df -h | /usr/bin/grep sda2 | /usr/bin/head -1 | /usr/bin/awk '{print $4}') | CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc"
+/usr/bin/printf "CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc"
+if [ -n "$(/usr/bin/lsmod | /usr/bin/grep nouveau)" ]; then
+    if [ "$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-17)" == '27' ];then
+        temp_gpu=$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/head -1 | /usr/bin/cut -c 16-17)
     else
-        /usr/bin/printf "CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc"
-        if [ -n "$(/usr/bin/lsmod | /usr/bin/grep nouveau)" ]; then
-            if [ "$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-17)" == '27' ];then
-                temp_gpu=$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/head -1 | /usr/bin/cut -c 16-17)
-            else
-                temp_gpu=$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-17)
-            fi
-            if [ $temp_gpu -ge 70 ];then
-                /usr/bin/printf "GPU: ""$(/usr/bin/echo 'ALERTA!')""\n"
-            else
-                /usr/bin/printf "GPU: ""$(/usr/bin/echo $temp_gpu)""ºc"""
-            fi
-        elif [ -n "$(/usr/bin/lsmod | /usr/bin/grep nvidia)" ]; then
-            temp_gpu=$(/usr/bin/nvidia-smi --query-gpu=temperature.gpu,utilization.gpu --format=csv,nounits,noheader | /usr/bin/cut -c1-2)
-            case $temp_gpu in
-                ''|*[!0-9]*) echo '' ;;
-                *) if [ $temp_gpu -ge 70 ];then
-                        /usr/bin/printf " | GPU: ""$(/usr/bin/echo 'ALERTA!')""\n"
-                else
-                        /usr/bin/printf " | GPU: ""$(/usr/bin/echo $temp_gpu)""ºc""\n"
-                fi
-                ;;  
-            esac
-        fi
+        temp_gpu=$(/usr/bin/sensors | /usr/bin/grep 'temp1:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-17)
     fi
+    if [ $temp_gpu -ge 70 ];then
+        /usr/bin/printf "GPU: ""$(/usr/bin/echo 'ALERTA!')""\n"
+    else
+        /usr/bin/printf "GPU: ""$(/usr/bin/echo $temp_gpu)""ºc"""
+    fi
+elif [ -n "$(/usr/bin/lsmod | /usr/bin/grep nvidia)" ]; then
+    temp_gpu=$(/usr/bin/nvidia-smi --query-gpu=temperature.gpu,utilization.gpu --format=csv,nounits,noheader | /usr/bin/cut -c1-2)
+    case $temp_gpu in
+        ''|*[!0-9]*) echo '' ;;
+        *) if [ $temp_gpu -ge 70 ];then
+                /usr/bin/printf " | GPU: ""$(/usr/bin/echo 'ALERTA!')""\n"
+        else
+                /usr/bin/printf " | GPU: ""$(/usr/bin/echo $temp_gpu)""ºc""\n"
+        fi
+        ;;
+    esac
 fi

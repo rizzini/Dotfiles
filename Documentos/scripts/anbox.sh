1eddcb46c92a5a59a8ecc10c1ad0ef04f9baf0bd
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-01-27 19:12:32 -0300

    atualizando

--- Documentos/scripts/anbox.sh
+++ Documentos/scripts/anbox.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if [ "$(/usr/bin/systemctl is-active anbox.service)" == 'active' ];then
     /usr/bin/sudo /usr/bin/systemctl stop anbox.service;
+    /usr/bin/sudo /usr/bin/systemctl stop anbox-container-manager.service
+
 else
-    /usr/bin/sudo /usr/bin/systemctl restart anbox.service;
+    /usr/bin/sudo /usr/bin/systemctl start anbox-container-manager.service
+    /usr/bin/sudo /usr/bin/systemctl start anbox.service;
+
 fi
--- Documentos/scripts/waydroid.sh
+++ Documentos/scripts/waydroid.sh
@@ -4,19 +4,17 @@ if [ "$(/usr/bin/systemctl is-active waydroid-container.service)" == 'active' ];
     /usr/bin/sudo /usr/bin/systemctl stop waydroid-container.service
     exit
 fi
-sudo /usr/local/bin/anbox-bridge restart
-/usr/bin/killall -9 weston
 /usr/bin/sudo /usr/bin/systemctl restart waydroid-container.service
-sudo /usr/local/bin/anbox-bridge restart
-
+/usr/bin/killall -9 weston
 if [ -z "$(/usr/bin/pgrep weston)" ]; then
     weston --width=780 --height=425 --xwayland &> /dev/null &
 fi
 /usr/bin/sleep 1 &&
 export XDG_SESSION_TYPE='wayland'
 export DISPLAY=':1'
+/bin/bash -c 'sleep 7 && sudo /usr/local/bin/anbox-bridge force-reload' &
 /usr/bin/konsole --new-tab --hide-tabbar --hide-menubar -e '/usr/bin/waydroid show-full-ui' &
 while [ -n "$(/usr/bin/pgrep weston)" ];do
     /usr/bin/sleep 1
-done
+donet
 /usr/bin/sudo /usr/bin/systemctl stop waydroid-container.service

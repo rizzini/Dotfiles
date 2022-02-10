d40b2e370bc0b4ebe83b3e0c05da09c93095d42c
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-10 18:10:18 -0300

    atualizando

--- Documentos/scripts/desativar_compositor.sh
+++ Documentos/scripts/desativar_compositor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/usr/bin/sleep 4 &&
 while [ "$(/usr/bin/qdbus org.kde.KWin /Compositor org.kde.kwin.Compositing.active)" == "true" ]; do
-    /usr/bin/xdotool keydown Alt_L+Shift_L + key F12 && /usr/bin/xdotool keyup Alt_L+Shift_L;
+#     /usr/bin/xdotool keydown Alt_L+Shift_L + key F12 && /usr/bin/xdotool keyup Alt_L+Shift_L;
+    qdbus org.kde.KWin /Compositor suspend;
     /usr/bin/sleep 1
 done
 

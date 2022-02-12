2b07e0bcf1a5e7c5544692672b0f3b5382b33ec3
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-12 20:04:34 -0300

    atualizando

---
+++ Documentos/scripts/change_wallpaper_every_boot.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+Backgrounds_Source="$HOME/Imagens/Wallpaper/"
+Random_Background=$(find "$Backgrounds_Source" -type f -print0 | xargs -0 file --mime-type | grep -F 'image/' | cut -d ':' -f 1 | sort -R | head -n 1)
+Background_Image="$HOME/.default_wallpaper.jpg"
+
+if [ -d "$Backgrounds_Source" ]; then
+
+    if [ -n "$Random_Background" ]; then
+
+	ln -Pfn "$Random_Background" "$Background_Image"
+
+    fi
+fi

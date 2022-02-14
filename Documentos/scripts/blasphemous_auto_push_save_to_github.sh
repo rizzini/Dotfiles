2ea4e5f14906799667cce9a5ea411e2cc99e1319
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-14 18:13:24 -0300

    atualizando

--- Documentos/scripts/blasphemous_auto_push_save_to_github.sh
+++ Documentos/scripts/blasphemous_auto_push_save_to_github.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /usr/bin/time -o /tmp/blasphemous.tmp -f "%e" /mnt/archlinux/Games/Blasphemous/start.sh &&
-if [ $(cat /tmp/blasphemous.tmp | awk -F. '{print $1}') -gt 600 ];then
-    sudo /usr/bin/yadm -Y /etc/yadm push;
-    rm -f /tmp/blasphemous.tmp
+if [ $(/usr/bin/cat /tmp/blasphemous.tmp | /usr/bin/awk -F. '{print $1}') -gt 1 ];then
+    echo -ne '\n' | /usr/bin/sudo /usr/bin/yadm -Y /etc/yadm push
+    /usr/bin/rm -f /tmp/blasphemous.tmp;
 fi

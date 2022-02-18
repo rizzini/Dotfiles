569414936c2e7004bf9db193b67c359a360878d9
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-18 06:04:44 -0300

    atualizando

--- Documentos/scripts/increase_descrease_volume.sh
+++ Documentos/scripts/increase_descrease_volume.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 step=3
 if [ -n "$1" ];then
+    mpg123 -q /home/lucas/Documentos/scripts/increase_descrease_volume.mp3 &
     if [ $1 == "increase" ];then
-        pactl -- set-sink-volume $(pactl get-default-sink) +$step%
+        pactl -- set-sink-volume $(pactl get-default-sink) +$step% &
     elif [ $1 == "decrease" ];then
-        pactl -- set-sink-volume $(pactl get-default-sink) -$step%
+        pactl -- set-sink-volume $(pactl get-default-sink) -$step% &
     fi
 fi

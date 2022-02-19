74b6a4d3b300b1f9a6172a6db13ad908a6a0e779
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-19 17:32:58 -0300

    atualizando

--- Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
+++ Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
@@ -2,7 +2,12 @@
 if test -h "/usr/share/wireplumber/main.lua.d/51-alsa-disable.lua"; then
     sudo /usr/bin/rm -f /usr/share/wireplumber/main.lua.d/51-alsa-disable.lua
     systemctl --user restart pipewire.service
-    pactl load-module module-combine-sink sink_name=Simultaneous slaves=alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1,alsa_output.pci-0000_00_1b.0.analog-stereo
+    sleep 1
+    pactl load-module module-null-sink media.class=Audio/Sink sink_name=Simultaneous channel_map=stereo
+    pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FL
+    pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FR
+    pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL
+    pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR
     pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo 100%
     pactl set-sink-volume alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1 100%
     pactl set-sink-volume Simultaneous 45%
@@ -12,6 +17,7 @@ else
     sleep 1
     pactl set-sink-volume alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1 50%
 fi
+
 
 # pactl load-module module-combine-sink sink_name=Simultaneous slaves=alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1,alsa_output.pci-0000_00_1b.0.analog-stereo
 

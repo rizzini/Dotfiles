655183ed473a4a5ca0b6968b797bbf3baf23581b
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-21 00:47:55 -0300

    atualizando

--- Documentos/scripts/increase_descrease_volume.sh
+++ Documentos/scripts/increase_descrease_volume.sh
@@ -2,7 +2,7 @@
 step=3
 if [ -n "$1" ];then
     if [ -z "$(pgrep spotify)" ];then
-        mpg123 -q /home/lucas/Documentos/scripts/increase_descrease_volume.mp3 &
+        mpg123 -q /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga/ &
     fi
     if [ $1 == "increase" ];then
         pactl -- set-sink-volume $(pactl get-default-sink) +$step% &

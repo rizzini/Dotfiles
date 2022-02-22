8526ecd965597bdb4a0f92131238c23f87ed450a
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-22 17:10:31 -0300

    atualizando

--- Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
+++ Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
@@ -9,7 +9,7 @@ if test -h "/usr/share/wireplumber/main.lua.d/51-alsa-disable.lua"; then
     /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL
     /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR
     /usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo 100%
-    /usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1 100%
+    /usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1 127%
     /usr/bin/pactl set-sink-volume Simultaneous 45%
 else
     /usr/bin/sudo /usr/bin/ln -s /usr/share/wireplumber/main.lua.d/disable_minisystem/51-alsa-disable.lua /usr/share/wireplumber/main.lua.d/51-alsa-disable.lua

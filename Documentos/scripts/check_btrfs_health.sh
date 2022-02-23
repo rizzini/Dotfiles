914f59e9ac319766c05cb186a8f28c611de0dad5
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-23 16:29:48 -0300

    atualizando

--- Documentos/scripts/check_btrfs_health.sh
+++ Documentos/scripts/check_btrfs_health.sh
@@ -4,7 +4,7 @@ if test -f "/tmp/check_btrfs_health.sh.tmp"; then
 fi
 /usr/bin/touch "/tmp/check_btrfs_health.sh.tmp";
 for i in $(/usr/bin/sudo /usr/bin/btrfs device stats / | tr -d '/dev/sda2' | grep -o -E '[0-9]+'); do
-    if [ "$i" != 0 ];then
+    if [ "$i" != "0" ];then
         XDG_RUNTIME_DIR=/run/user/1000 DISPLAY=:0 /usr/bin/kdialog --error "$(/usr/bin/sudo /usr/bin/btrfs device stats /)" --title='Device IO error';
         break
     fi

ab5dd40935fbf905db8e4b53b8e928ad15033c96
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-23 19:08:37 -0300

    atualizando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,26 +2,26 @@ set fish_greeting;
 set VIRTUAL_ENV_DISABLE_PROMPT "1";
 source ~/.profile;
 
-set XORG (grep nvidia /etc/X11/xorg.conf)
-if test -n "$DISPLAY";
-    if ! set -q $XORG;
-        if ! string match -q "*opengl_libs*" $LD_LIBRARY_PATH
-            set -x LD_LIBRARY_PATH "$LD_LIBRARY_PATH:/mnt/archlinux/opengl_libs/nvidia/";
-        end
-        if test -d "$HOME/.cache/";
-            export __GL_SHADER_DISK_CACHE_PATH="$HOME/.cache/";
-        else
-            mkdir -p $HOME/.cache/
-            export __GL_SHADER_DISK_CACHE_PATH="$HOME/.cache/";
-        end
-        export KWIN_TRIPLE_BUFFER=1;
-        export __GL_YIELD=USLEEP;
-        export __GL_MaxFramesAllowed=1;
-        export KWIN_COMPOSE=O2ES;
-        export __GL_THREADED_OPTIMIZATION=1;
-        export __GL_SHADER_DISK_CACHE=1;
-    end
-end
+# set XORG (grep nvidia /etc/X11/xorg.conf)
+# if test -n "$DISPLAY";
+#     if ! set -q $XORG;
+#         if ! string match -q "*opengl_libs*" $LD_LIBRARY_PATH
+#             set -x LD_LIBRARY_PATH "$LD_LIBRARY_PATH:/mnt/archlinux/opengl_libs/nvidia/";
+#         end
+#         if test -d "$HOME/.cache/";
+#             export __GL_SHADER_DISK_CACHE_PATH="$HOME/.cache/";
+#         else
+#             mkdir -p $HOME/.cache/
+#             export __GL_SHADER_DISK_CACHE_PATH="$HOME/.cache/";
+#         end
+#         export KWIN_TRIPLE_BUFFER=1;
+#         export __GL_YIELD=USLEEP;
+#         export __GL_MaxFramesAllowed=1;
+#         export KWIN_COMPOSE=O2ES;
+#         export __GL_THREADED_OPTIMIZATION=1;
+#         export __GL_SHADER_DISK_CACHE=1;
+#     end
+# end
 
 
 # export PATH="/opt/VirtualGL/bin/:$PATH";
@@ -39,8 +39,6 @@ alias ls='exa -al --color=always --group-directories-first --icons';
 alias grep='grep --color=auto';
 alias big="expac -H M '%m\t%n' | sort -h | nl";
 alias gitpkg='pacman -Q | grep -i "\-git" ';
-# alias dolphin='dolphin &> /dev/null & disown $last_pid';
-alias jctl="journalctl -p 3 -xb";
 alias syadm="sudo yadm -Y /etc/yadm";
 alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl";
 alias cat='bat -p ';

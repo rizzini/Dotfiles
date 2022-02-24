f1a0882e946589c7f14d32bbe64db142d0686188
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-24 17:02:41 -0300

    atualizando

--- .config/fish/functions/d.fish
+++ .config/fish/functions/d.fish
@@ -3,7 +3,5 @@ function d
         /usr/bin/dolphin . &> /dev/null & disown $last_pid
     else if [ -n "$argv[1]" ]
         /usr/bin/dolphin "$argv[1]" &> /dev/null & disown $last_pid
-    else if [ -z "$argv[1]" ]
-        /usr/bin/dolphin &> /dev/null & disown $last_pid
     end
 end

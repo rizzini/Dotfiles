2e2597ee5e0ff2641c227c2f57ec96f2aeb204fb
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-26 23:52:52 -0300

    atualizando

--- Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
+++ Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [[ $(uptime -p | awk '{print $2}' | tr -d ' ' | tr -d '\n') -eq 0 && ! -h "/etc/wireplumber/main.lua.d/51-alsa-disable.lua" ]];then
+    /usr/bin/sleep 5;
     /usr/bin/pactl load-module module-null-sink media.class=Audio/Sink sink_name=Simultaneous channel_map=stereo;
     /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FL;
     /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FR;
     /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL;
     /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR;
+    /usr/bin/pactl set-default-sink Simultaneous;
 else
     if test -h "/etc/wireplumber/main.lua.d/51-alsa-disable.lua"; then
         /usr/bin/sudo /usr/bin/rm -f /etc/wireplumber/main.lua.d/51-alsa-disable.lua;

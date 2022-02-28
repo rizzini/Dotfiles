cda44ebe97e8130e04ba0f2eb5af84278a49adad
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-27 21:42:54 -0300

    atualizando

--- Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
+++ Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 create_sink_and_link(){
-    usr/bin/pactl load-module module-null-sink media.class=Audio/Sink sink_name=Simultaneous channel_map=stereo;
+    /usr/bin/pactl load-module module-null-sink media.class=Audio/Sink sink_name=Simultaneous channel_map=stereo;
     /usr/bin/sleep 1;
     /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FL;
     /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FR;
     /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL;
     /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR;
 }
-
-if [ "$1" == "" ];then
-    create_sink_and_link
-fi
 
 if [[ $(/usr/bin/uptime -p | /usr/bin/awk '{print $2}' | /usr/bin/tr -d ' ' | /usr/bin/tr -d '\n') -eq 0 && ! -h "/etc/wireplumber/main.lua.d/51-alsa-disable.lua" ]];then
     /usr/bin/sleep 5;

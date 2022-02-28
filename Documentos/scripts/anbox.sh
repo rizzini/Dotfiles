576e66148c8e84640fa6bedc8c75c95c1cfcc38b
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-02-28 10:03:33 -0300

    atualizando

--- Documentos/scripts/anbox.sh
+++ Documentos/scripts/anbox.sh
@@ -2,22 +2,22 @@
 export XDG_RUNTIME_DIR=/run/user/1000
 export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 export DISPLAY=:0
-if [[ -n "$1" && -n "$(DISPLAY=:0 /usr/bin/wmctrl -l | grep  'Prime Video')"  ]];then
-    killall -9 anbox
+if [[ -n "$1" && -n "$(DISPLAY=:0 /usr/bin/wmctrl -l | /usr/bin/grep  'Prime Video')"  ]];then
+    /usr/bin/killall -9 anbox
     /usr/bin/anbox session-manager --single-window --window-size=600,507 &
     /usr/bin/sleep 1
     /usr/bin/wmctrl -r 'Anbox - Android in a Box' -e 0,961,543,-1,-1
     while [ "$(pgrep anbox)" ];do
-        sleep 2;
+        /usr/bin/sleep 2;
     done
 
-elif [[ -n "$1" && -z "$(DISPLAY=:0 /usr/bin/wmctrl -l | grep  'Prime Video')"  ]];then
-    killall -9 anbox
+elif [[ -n "$1" && -z "$(DISPLAY=:0 /usr/bin/wmctrl -l | /usr/bin/grep  'Prime Video')"  ]];then
+    /usr/bin/killall -9 anbox
     /usr/bin/anbox session-manager --single-window --window-size=600,524 &
     /usr/bin/sleep 1
     /usr/bin/wmctrl -r 'Anbox - Android in a Box' -e 0,961,525,-1,-1
     while [ "$(pgrep anbox)" ];do
-        sleep 2;
+        /usr/bin/sleep 2;
     done
 fi
 
@@ -27,7 +27,7 @@ if [[ -z "$1" &&  "$(/usr/bin/systemctl is-active anbox.service)" == 'active' ]]
 
 elif [[ -z "$1" &&  "$(/usr/bin/systemctl is-active anbox.service)" == 'inactive' ]];then
     /usr/bin/sudo /usr/bin/systemctl start anbox-container-manager.service
-    sleep 1
+    /usr/bin/sleep 1
     /usr/bin/sudo /usr/bin/systemctl start anbox.service;
 
 fi

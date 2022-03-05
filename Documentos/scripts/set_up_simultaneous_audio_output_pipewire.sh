09b24cf2dfb9a7bcdfb13c81cb1472f7ab3fef71
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-03-05 17:43:13 -0300

    atualizando

--- Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
+++ Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
@@ -7,12 +7,16 @@ if [ ! -h "/etc/wireplumber/main.lua.d/51-alsa-disable.lua" ];then
 elif [ -h "/etc/wireplumber/main.lua.d/51-alsa-disable.lua" ]; then
     /usr/bin/sudo /usr/bin/rm -f /etc/wireplumber/main.lua.d/51-alsa-disable.lua;
     /usr/bin/systemctl --user restart pipewire.service;
+    /isr/bin/sleep 1
     if [ -z "$(pw-cli list-objects Node | /usr/bin/grep Simultaneous)" ];then
         /usr/bin/pactl load-module module-null-sink media.class=Audio/Sink sink_name=Simultaneous channel_map=stereo;
         /usr/bin/sleep 1;
         /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FL;
+        /usr/bin/sleep 1;
         /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FR;
+        /usr/bin/sleep 1;
         /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL;
+        /usr/bin/sleep 1;
         /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR;
     fi
     /usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo 85%;

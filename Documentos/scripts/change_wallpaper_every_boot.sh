6b59cc16d57e7e26a26c899454a37000bf1ca7f9
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-03-19 18:13:39 -0300

    atualizando

--- Documentos/scripts/change_wallpaper_every_boot.sh
+++ Documentos/scripts/change_wallpaper_every_boot.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/bin/bash
 Backgrounds_Source="$HOME/Imagens/Wallpaper/"
 Random_Background=$(find "$Backgrounds_Source" -type f -print0 | xargs -0 file --mime-type | grep -F 'image/' | cut -d ':' -f 1 | sort -R | head -n 1)
 Background_Image="$HOME/.default_wallpaper.jpg"

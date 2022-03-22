69e5d162aac9717d8a67ac493d34840bef060e62
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-03-22 16:49:14 -0300

    atualizando

--- Documentos/scripts/clean_space_emergency.sh
+++ Documentos/scripts/clean_space_emergency.sh
@@ -4,9 +4,14 @@ find /mnt/archlinux/btrbk_snapshots/HOME/* -prune -type d | while IFS= read -r d
         btrfs sub del "$d"
     fi
 done
+find /mnt/archlinux/btrbk_snapshots/ROOT/* -prune -type d | while IFS= read -r d; do
+    if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep ROOT | head -3 | tail -1 |  awk '{print $9}')"* ]]; then
+        btrfs sub del "$d"
+    fi
+done
 find /mnt/archlinux/refind_btrfs_rw_snapshots/* -prune -type d | while IFS= read -r d; do
     if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep refind_btrfs_rw_snapshots | head -3 | tail -1 |  awk '{print $9}')"* ]]; then
-        btrfs sub del "$d"
+        btrfs sub del  "$d"
     fi
 done
 if [ -z "$(pgrep spotify)" ];then

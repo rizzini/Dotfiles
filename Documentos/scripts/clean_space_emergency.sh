7b11a8705c0fd2ee8df30061b91228676ad3e58b
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-03-26 05:43:19 -0300

    atualizando

--- Documentos/scripts/clean_space_emergency.sh
+++ Documentos/scripts/clean_space_emergency.sh
@@ -8,17 +8,18 @@ if [ "$EUID" -ne 0 ]; then
 fi
 touch /tmp/clean_space_emergency.tmp
 find /mnt/archlinux/btrbk_snapshots/HOME/* -prune -type d | while IFS= read -r d; do
-    if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep HOME | head -3 | tail -1 |  awk '{print $9}')"* ]]; then
+    if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep HOME | awk '{print $9}' | tail -1)"* ]]; then
         btrfs sub del "$d"
     fi
 done
 find /mnt/archlinux/btrbk_snapshots/ROOT/* -prune -type d | while IFS= read -r d; do
-    if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep ROOT | head -3 | tail -1 |  awk '{print $9}')"* ]]; then
+    if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep ROOT | awk '{print $9}' | tail -1)"* ]]; then
         btrfs sub del "$d"
+
     fi
 done
 find /mnt/archlinux/refind_btrfs_rw_snapshots/* -prune -type d | while IFS= read -r d; do
-    if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep refind_btrfs_rw_snapshots | head -3 | tail -1 |  awk '{print $9}')"* ]]; then
+    if [[ -d "$d" && "$d" != *"$(btrfs subvol list / | grep refind_btrfs_rw_snapshots | awk '{print $9}' | tail -1)"* ]]; then
         btrfs sub del "$d"
     fi
 done
@@ -26,7 +27,7 @@ if [ -z "$(pgrep spotify)" ];then
     rm -rf /home/lucas/.cache/spotify/
 fi
 if [ -z "$(pgrep makepkg)" ];then
-    rm -rf /mnt/archlinux/CCACHE/*
+    rm -rf /home/lucas/.ccache/*
     if [ -z "$(pgrep yay)" ];then
         rm -rf /home/lucas/.cache/yay/*
     fi

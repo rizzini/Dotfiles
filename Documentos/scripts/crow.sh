578c5f25c2fce927227c78c29830a151992db4b2
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-03-27 19:10:21 -0300

    atualizando

--- Documentos/scripts/crow.sh
+++ Documentos/scripts/crow.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 if [ -z $(pgrep hl2_linux) ];then
-    crow & sleep 0.8;
-    while [ $(wmctrl -l | grep 'Crow Translate' | wc -l) -ge 2 ];do
-        wmctrl -ic $(wmctrl -l | grep 'Crow Translate' | tail -1 | awk '{print $1}');
-    done
+    dbus-send --type=method_call --dest=io.crow_translate.CrowTranslate /io/crow_translate/CrowTranslate/MainWindow io.crow_translate.CrowTranslate.MainWindow.open
 fi

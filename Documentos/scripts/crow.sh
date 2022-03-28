676ca603723e231f87cf570ef8033bbad34b3568
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-03-28 07:02:45 -0300

    atualizando

--- Documentos/scripts/crow.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [ -z $(pgrep hl2_linux) ];then
-    dbus-send --type=method_call --dest=io.crow_translate.CrowTranslate /io/crow_translate/CrowTranslate/MainWindow io.crow_translate.CrowTranslate.MainWindow.open
-fi

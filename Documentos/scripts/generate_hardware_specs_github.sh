664164ccb8554f9ff54db93037589f6e6387f31b
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-03-30 10:59:53 -0300

    atualizando

--- Documentos/scripts/generate_hardware_specs_github.sh
+++ Documentos/scripts/generate_hardware_specs_github.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 if [ "$EUID" -ne 0 ]; then
-    /usr/bin/echo "Please run as root";
+    /usr/bin/echo "Root needed";
     exit
 fi
 export LC_ALL=C
-fdisk -l | tee '/home/lucas/Documentos/hardware_specs/fdisk -l'
-glxinfo -B | tee '/home/lucas/Documentos/hardware_specs/glxinfo -B'
-hwinfo | tee '/home/lucas/Documentos/hardware_specs/hwinfo'
-inxi -F | tee '/home/lucas/Documentos/hardware_specs/inxi -F'
-lsblk | tee '/home/lucas/Documentos/hardware_specs/lsblk'
-lscpu | tee '/home/lucas/Documentos/hardware_specs/lscpu'
-lshw | tee '/home/lucas/Documentos/hardware_specs/lshw'
-lspci -v | tee '/home/lucas/Documentos/hardware_specs/lspci -v'
-lsusb -v | tee '/home/lucas/Documentos/hardware_specs/lsusb -v'
-vainfo | tee '/home/lucas/Documentos/hardware_specs/vainfo'
-vulkaninfo | tee '/home/lucas/Documentos/hardware_specs/vulkaninfo'
-btrfs device usage / | tee '/home/lucas/Documentos/hardware_specs/btrfs device usage'
-btrfs filesystem df / | tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem df'
-btrfs filesystem show | tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem show'
-btrfs filesystem usage / | tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem usage'
-sudo -u lucas yay -Q | tee '/home/lucas/Documentos/hardware_specs/installed_package_list'
+/usr/bin/fdisk -l | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/fdisk -l'
+/usr/bin/glxinfo -B | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/glxinfo -B'
+/usr/bin/hwinfo | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/hwinfo'
+/usr/bin/inxi -F | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/inxi -F'
+/usr/bin/lsblk | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/lsblk'
+/usr/bin/lscpu | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/lscpu'
+/usr/bin/lshw | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/lshw'
+/usr/bin/lspci -v | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/lspci -v'
+/usr/bin/lsusb -v | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/lsusb -v'
+/usr/bin/vainfo | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/vainfo'
+/usr/bin/vulkaninfo | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/vulkaninfo'
+/usr/bin/btrfs device usage / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs device usage'
+/usr/bin/btrfs filesystem df / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem df'
+/usr/bin/btrfs filesystem show | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem show'
+/usr/bin/btrfs filesystem usage / | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/btrfs filesystem usage'
+/usr/bin/sudo -u lucas /usr/bin/yay -Q | /usr/bin/tee '/home/lucas/Documentos/hardware_specs/installed_package_list'

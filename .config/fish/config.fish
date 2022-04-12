fe44c3ffc8e01f3eebe6e3253e961555fd278c10
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-04-12 06:16:27 -0300

    atualizando

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set fish_greeting;
-source ("/usr/bin/starship" init fish --print-full-init | psub);
+# source ("/usr/bin/starship" init fish --print-full-init | psub);
+starship init fish | source
 set VIRTUAL_ENV_DISABLE_PROMPT "1";
 source ~/.profile;
 # export PATH="/opt/VirtualGL/bin/:$PATH";

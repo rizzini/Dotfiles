05dcb5c8669641c091aa92b81fcf1fd492475e91
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-04-22 12:59:20 -0300

    atualizando

--- Documentos/scripts/clean_space_emergency.sh
+++ Documentos/scripts/clean_space_emergency.sh
@@ -6,6 +6,7 @@ if [ "$EUID" -ne 0 ]; then
     /usr/bin/echo "Please run as root";
     exit
 fi
+{
 /usr/bin/touch /tmp/clean_space_emergency.tmp
 /usr/bin/find /mnt/archlinux/btrbk_snapshots/HOME/* -prune -type d | while IFS= read -r d; do
     if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list / | /usr/bin/grep HOME | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
@@ -39,3 +40,4 @@ fi
 if [ -z "$(/usr/bin/pgrep winetricks)" ];then
     /usr/bin/rm -rf /home/lucas/.cache/winetricks/*
 fi
+} &> /dev/null

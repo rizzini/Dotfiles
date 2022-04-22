c50c0c277186a3cac4bb8e0062deef7087210084
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-04-22 13:04:15 -0300

    atualizando

--- Documentos/scripts/ativar_desativar_compositor.sh
+++ Documentos/scripts/ativar_desativar_compositor.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bashzz
 if [ "$1" == "desativar" ]; then
     /usr/bin/qdbus org.kde.KWin /Compositor suspend;
-elif [ "$1" == "ativar" ]; then
+elif [[ "$1" == "ativar" && ! -f "/home/lucas/Documentos/scripts/compositor_off" ]]; then
     /usr/bin/qdbus org.kde.KWin /Compositor resume;
 fi
 

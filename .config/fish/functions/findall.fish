fef4d8ef7666f0572283b7d666964b889def4b05
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-05-30 08:37:10 -0300

    atualizando

--- .config/fish/functions/findall.fish
+++ .config/fish/functions/findall.fish
@@ -1,3 +1,3 @@
 function findall
-    sudo find / -path '/mnt/archlinux' -prune -o -iname "*$argv[1]*" 2> /dev/null | head -n -1
+    sudo find / -path '/mnt' -prune -o -iname "*$argv[1]*" 2> /dev/null | head -n -1
 end

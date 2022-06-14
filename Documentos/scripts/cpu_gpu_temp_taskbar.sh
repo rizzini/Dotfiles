e61814771168e88156ecc4633af26372ac6d268c
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-06-14 08:43:31 -0300

    atualizando

--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -16,6 +16,8 @@ elif [ -n "$(pgrep easyeffects)" ];then
 else
     if [ $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18) -ge 80 ];then
         /usr/bin/printf "CPU: ""$(/usr/bin/echo $(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18))""ºc";
+    elif [ "$(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 17-18)" == '00' ];then
+        /usr/bin/printf "CPU: ""$(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-18)""ºc";
     fi
 fi
 clean_space_emergency;

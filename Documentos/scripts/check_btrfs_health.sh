9349a2b589a9c66de520813c35542d5eea4b0f07
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-06-30 18:14:34 -0300

    atualizando

--- Documentos/scripts/check_btrfs_health.sh
+++ Documentos/scripts/check_btrfs_health.sh
@@ -5,6 +5,18 @@ fi
 /usr/bin/touch "/tmp/check_btrfs_health.sh.tmp";
 /usr/bin/sudo /usr/bin/btrfs device stats -c /;
 if [ $? != 0 ];then
-    DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/kdialog --error "$(/usr/bin/sudo /usr/bin/btrfs device stats /)" --title='Device IO error';
+    DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/kdialog --error "$(/usr/bin/sudo /usr/bin/btrfs device stats /)" --title='Arch: Device IO error';
+fi
+if [ -n "$(mount | grep '/mnt/dados')" ];then
+    /usr/bin/sudo /usr/bin/btrfs device stats -c /mnt/dados/;
+    if [ $? != 0 ];then
+        DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/kdialog --error "$(/usr/bin/sudo /usr/bin/btrfs device stats /)" --title='Dados: Device IO error';
+    fi
+fi
+if [ -n "$(mount | grep '/mnt/backup')" ];then
+    /usr/bin/sudo /usr/bin/btrfs device stats -c /mnt/backup/;
+    if [ $? != 0 ];then
+        DISPLAY=:0 XDG_RUNTIME_DIR=/run/user/1000 /usr/bin/kdialog --error "$(/usr/bin/sudo /usr/bin/btrfs device stats /)" --title='Backup: Device IO error';
+    fi
 fi
 /usr/bin/rm -f "/tmp/check_btrfs_health.sh.tmp";
--- Documentos/scripts/cpu_gpu_temp_taskbar.sh
+++ Documentos/scripts/cpu_gpu_temp_taskbar.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-clean_space_emergency(){
-    if [ $(/usr/bin/df -B MB  /dev/sda2 --output=avail | /usr/bin/tail -1 | /usr/bin/tr -d 'MB') -le 600 ];then
-        /usr/bin/sudo /home/lucas/Documentos/scripts/clean_space_emergency.sh &
-        /usr/bin/sleep 20;
-    fi
-}
-
 /usr/bin/grep 'ENABLED=no' /etc/ufw/ufw.conf &> /dev/null
 if [ $? -eq 0 ];then
     /usr/bin/echo 'Firewall desativado';
-    clean_space_emergency;
+    if [ $(/usr/bin/df -B MB  /dev/sda2 --output=avail | /usr/bin/tail -1 | /usr/bin/tr -d 'MB') -le 600 ];then
+    /usr/bin/sudo /home/lucas/Documentos/scripts/clean_space_emergency.sh &
+    /usr/bin/sleep 20;
+    fi
     exit 0
 elif [ -n "$(pgrep easyeffects)" ];then
     echo 'EasyEffects ligado'
@@ -20,4 +16,7 @@ else
         /usr/bin/printf "CPU: ""$(/usr/bin/sensors | /usr/bin/grep 'Package id 0:' | /usr/bin/tail -1 | /usr/bin/cut -c 16-18)""ºc";
     fi
 fi
-clean_space_emergency;
+if [ $(/usr/bin/df -B MB  /dev/sda2 --output=avail | /usr/bin/tail -1 | /usr/bin/tr -d 'MB') -le 600 ];then
+    /usr/bin/sudo /home/lucas/Documentos/scripts/clean_space_emergency.sh &
+    /usr/bin/sleep 20;
+fi

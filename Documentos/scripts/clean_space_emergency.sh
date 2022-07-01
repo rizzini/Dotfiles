4782aa054cefce14636ba45062a1da56b211ae9a
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-07-01 19:34:49 -0300

    atualizando

--- Documentos/scripts/clean_space_emergency.sh
+++ Documentos/scripts/clean_space_emergency.sh
@@ -4,47 +4,47 @@ while :;do
     {
     if [ $(/usr/bin/df -B MB  /dev/sda2 --output=avail | /usr/bin/tail -1 | /usr/bin/tr -d 'MB') -le 700 ];then
         out_of_space=1
-#         /usr/bin/find /mnt/archlinux/btrbk_snapshots/HOME/* -prune -type d | while IFS= read -r d; do
-#             if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list / | /usr/bin/grep HOME | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
-#                 /usr/bin/btrfs sub del "$d"
-#             fi
-#         done
-#         /usr/bin/find /mnt/archlinux/btrbk_snapshots/ROOT/* -prune -type d | while IFS= read -r d; do
-#             if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list / | /usr/bin/grep ROOT | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
-#                 /usr/bin/btrfs sub del "$d"
-#             fi
-#         done
-#         /usr/bin/find /mnt/archlinux/refind_btrfs_rw_snapshots/* -prune -type d | while IFS= read -r d; do
-#             if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list / | /usr/bin/grep refind_btrfs_rw_snapshots | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
-#                 /usr/bin/btrfs sub del "$d"
-#             fi
-#         done
-#         if [ -z "$(/usr/bin/pgrep spotify)" ];then
-#             /usr/bin/rm -rf /home/lucas/.cache/spotify/
-#         fi
-#         if [ -z "$(/usr/bin/pgrep makepkg)" ];then
-#             /usr/bin/rm -rf /home/lucas/.ccache/*
-#             if [ -z "$(/usr/bin/pgrep yay)" ];then
-#                 yay -Scc --noconfirm
-#             fi
-#         fi
-#         if [ -z "$(/usr/bin/pgrep pacman)" ];then
-#             /usr/bin/rm -rf /mnt/archlinux/PKG/*
-#         fi
-#         if [ -z "$(/usr/bin/pgrep winetricks)" ];then
-#             /usr/bin/rm -rf /home/lucas/.cache/winetricks/*
+        /usr/bin/find /mnt/archlinux/btrbk_snapshots/HOME/* -prune -type d | while IFS= read -r d; do
+            if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list / | /usr/bin/grep HOME | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
+                /usr/bin/btrfs sub del "$d"
+            fi
+        done
+        /usr/bin/find /mnt/archlinux/btrbk_snapshots/ROOT/* -prune -type d | while IFS= read -r d; do
+            if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list / | /usr/bin/grep ROOT | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
+                /usr/bin/btrfs sub del "$d"
+            fi
+        done
+        /usr/bin/find /mnt/archlinux/refind_btrfs_rw_snapshots/* -prune -type d | while IFS= read -r d; do
+            if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list / | /usr/bin/grep refind_btrfs_rw_snapshots | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
+                /usr/bin/btrfs sub del "$d"
+            fi
+        done
+        if [ -z "$(/usr/bin/pgrep spotify)" ];then
+            /usr/bin/rm -rf /home/lucas/.cache/spotify/
+        fi
+        if [ -z "$(/usr/bin/pgrep makepkg)" ];then
+            /usr/bin/rm -rf /home/lucas/.ccache/*
+            if [ -z "$(/usr/bin/pgrep yay)" ];then
+                /usr/bin/yay -Scc --noconfirm
+            fi
+        fi
+        if [ -z "$(/usr/bin/pgrep pacman)" ];then
+            /usr/bin/rm -rf /mnt/archlinux/PKG/*
+        fi
+        if [ -z "$(/usr/bin/pgrep winetricks)" ];then
+            /usr/bin/rm -rf /home/lucas/.cache/winetricks/*
     fi
     if [ $(/usr/bin/df -B MB  /dev/sdb2 --output=avail | /usr/bin/tail -1 | /usr/bin/tr -d 'MB') -le 700 ];then
         out_of_space=1
-#         killall -9 btrbk_ btrbk
-#         /usr/bin/find /mnt/backup/ROOT/* -prune -type d | while IFS= read -r d; do
-#             if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list /mnt/backup/ROOT/ | /usr/bin/grep ROOT | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
-#                 /usr/bin/btrfs sub del "$d"
-#             fi
-#         /usr/bin/find /mnt/backup/HOME/* -prune -type d | while IFS= read -r d; do
-#             if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list /mnt/backup/HOME/ | /usr/bin/grep HOME | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
-#                 /usr/bin/btrfs sub del "$d"
-#             fi
+        killall -9 btrbk_ btrbk
+        /usr/bin/find /mnt/backup/ROOT/* -prune -type d | while IFS= read -r d; do
+            if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list /mnt/backup/ROOT/ | /usr/bin/grep ROOT | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
+                /usr/bin/btrfs sub del "$d"
+            fi
+        /usr/bin/find /mnt/backup/HOME/* -prune -type d | while IFS= read -r d; do
+            if [[ -d "$d" && "$d" != *"$(/usr/bin/btrfs subvol list /mnt/backup/HOME/ | /usr/bin/grep HOME | /usr/bin/awk '{print $9}' | /usr/bin/tail -1)"* ]]; then
+                /usr/bin/btrfs sub del "$d"
+            fi
     fi
     } &> /tmp/clean_space_emergency.sh.log
 if [ $out_of_space = 1 ];then

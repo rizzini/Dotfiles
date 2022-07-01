683479a3927ec214ead341579db96494147adc8b
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-07-01 20:51:32 -0300

    atualizando

--- Documentos/scripts/easyeffects_in_background.sh
+++ Documentos/scripts/easyeffects_in_background.sh
@@ -3,5 +3,6 @@
 if [[ -n "$(pgrep easyeffects)" && "$(pgrep easyeffects | wc -l)" -lt 2 ]];then
     killall easyeffects;
 else
-    easyeffects --gapplication-service &> /dev/null & disown $!
+    easyeffects --gapplication-service &> /dev/null &
+    easyeffects -l 'LoudnessEqualizer' &> /dev/null &
 fi

c4732e7aca45a50f6933ea91b5bd8e249d65792a
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-07-06 21:39:59 -0300

    atualizando

--- Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
+++ Documentos/scripts/set_up_simultaneous_audio_output_pipewire.sh
@@ -18,35 +18,35 @@ elif [ -h "/etc/wireplumber/main.lua.d/51-alsa-disable.lua" ]; then
     #pactl load-module module-combine-sink sink_name=combination-sink sink_properties=device.description=Saída_Combinada slaves=alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1,alsa_output.pci-0000_00_1b.0.analog-stereo channels=2
     while ! /usr/bin/pw-cli list-objects Node | /usr/bin/grep -q Simultaneous; do
         /usr/bin/pactl load-module module-null-sink media.class=Audio/Sink sink_name=Simultaneous channel_map=stereo;
-        sleep 0.2
+        /usr/bin/sleep 0.2
     done
-    sleep 0.1
-    while ! pw-link -l | grep -q 'alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FL';do
+    /usr/bin/sleep 0.1
+    while ! pw-link -l | /usr/bin/grep -q 'alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FL';do
         /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FL;
-        sleep 0.2
+        /usr/bin/sleep 0.2
     done
-    sleep 0.1
-    while ! pw-link -l | grep -q 'alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FR';do
+    /usr/bin/sleep 0.1
+    while ! pw-link -l | /usr/bin/grep -q 'alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FR';do
         /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1:playback_FR;
-        sleep 0.2
+        /usr/bin/sleep 0.2
     done
-    sleep 0.1
-    while ! pw-link -l | grep -q 'alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL';do
+    /usr/bin/sleep 0.1
+    while ! pw-link -l | /usr/bin/grep -q 'alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL';do
         /usr/bin/pw-link Simultaneous:monitor_FL alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FL;
-        sleep 0.2
+        /usr/bin/sleep 0.2
     done
-    sleep 0.1
-    while ! pw-link -l | grep -q 'alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR';do
+    /usr/bin/sleep 0.1
+    while ! pw-link -l | /usr/bin/grep -q 'alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR';do
         /usr/bin/pw-link Simultaneous:monitor_FR alsa_output.pci-0000_00_1b.0.analog-stereo:playback_FR;
-        sleep 0.2
+        /usr/bin/sleep 0.2
     done
-    sleep 0.2
+    /usr/bin/sleep 0.2
     /usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_1b.0.analog-stereo 100%;
     /usr/bin/pactl set-sink-volume alsa_output.pci-0000_00_03.0.hdmi-stereo-extra1 100%;
     /usr/bin/pactl set-sink-volume Simultaneous "$get_current_volume";
     /usr/bin/pactl set-default-sink Simultaneous;
 fi
-if [ $pulseeffects == 1  ];then
-    sleep 1
+if [ $pulseeffects == 1 ];then
+    /usr/bin/sleep 1
     /home/lucas/Documentos/scripts/easy.effects_in_background.sh
 fi

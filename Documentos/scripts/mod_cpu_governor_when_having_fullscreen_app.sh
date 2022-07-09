cf99a9954ee16e29b0963788f8f18bd9d484e957
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-07-08 21:56:47 -0300

    atualizando

--- Documentos/scripts/mod_cpu_governor_when_having_fullscreen_app.sh
+++ Documentos/scripts/mod_cpu_governor_when_having_fullscreen_app.sh
@@ -2,7 +2,7 @@
 #For when watching movies and such..
 /usr/bin/sleep 10
 while :; do
-    if [[ "$(/usr/bin/xdotool getactivewindow getwindowgeometry | /usr/bin/grep 'Geometry:' | /usr/bin/awk '{print $2}')" == '1920x1080' && "$(/usr/bin/xdotool getactivewindow getwindowclassname)" != 'plasmashell' && -z "$(pgrep wine)" ]]; then
+    if [[ "$(/usr/bin/xdotool getactivewindow getwindowgeometry | /usr/bin/grep 'Geometry:' | /usr/bin/awk '{print $2}')" == '1920x1080' && "$(/usr/bin/xdotool getactivewindow getwindowclassname)" != 'plasmashell' && -z "$(pgrep wine)" && -z "$(pgrep makepkg)"  ]]; then
         fullscreen=1;
         sleep=1;
     else
@@ -12,11 +12,9 @@ while :; do
     if [ $fullscreen == 1 ] && ! /usr/bin/grep -q 'powersave' /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor; then
         /usr/bin/echo -n 'powersave' | /usr/bin/sudo /usr/bin/tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
         /usr/bin/mpv --no-terminal /home/lucas/Documentos/scripts/mod_cpu_governor_when_having_fullscreen_app.mp3; #Remove after stating the script is reliable.
-        exit
     elif [ $fullscreen == 0 ] && ! /usr/bin/grep -q 'performance' /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor; then
         /usr/bin/echo -n 'performance' | /usr/bin/sudo /usr/bin/tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
         /usr/bin/mpv --no-terminal /home/lucas/Documentos/scripts/mod_cpu_governor_when_having_fullscreen_app.mp3; #Remove after stating the script is reliable.
-        exit
     fi
     /usr/bin/sleep $sleep;
 done

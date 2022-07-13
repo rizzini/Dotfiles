8b32181074f62a07c6648c42a2de6db953b4c10a
Lucas Rizzini <lucasrizzini@hotmail.com>
2022-07-13 16:13:01 -0300

    atualizando

--- Documentos/scripts/nightstand_mouse_volume_mod.sh
+++ Documentos/scripts/nightstand_mouse_volume_mod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 device_id=$(xinput --list | grep '2.4G Mouse' |  awk '{print $5}' | tr -d 'id=')
 xinput --test $device_id | grep --line-buffered -E 'button press   1|button press   3|button release 1|button release 3' | while read -r line; do
-if [ "$line" == 'button press   1' ] && [ "$right_button" == 0 ]; then
+    if [ "$line" == 'button press   1' ] && [ "$right_button" == 0 ]; then
         left_button=1
         continue
     elif [ "$line" == 'button release 1' ]; then
